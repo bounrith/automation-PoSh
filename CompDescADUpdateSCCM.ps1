2cfaa4b3c99ad0bddeedfba33f8d79be458d9d0d
Bounrith Ly <bounrith@hotmail.com>
2023-01-12 21:14:57 -0800

    Update CompDescADUpdateSCCM.ps1

--- CompDescADUpdateSCCM.ps1
+++ CompDescADUpdateSCCM.ps1
@@ -10,9 +10,14 @@
 $PO = ""
 
 # GET LOCAL COMPUTER DETAILS
-# TO DO: Use modern method instead of WmiObject
+# TO DO: Use CIMinstance instead of WmiObject
+# VERSION 2: $ComputerName = Get-CIMinstance -Class Win32_ComputerSystem -Property Name
+
+
 $ComputerName = (Get-WmiObject -Class Win32_ComputerSystem -Property Name).Name
 $SerialNumber = (Get-WmiObject -class win32_bios).SerialNumber
+
+# VERSION2: # $CurrDesc = Get-ADComputer -Identity $ComputerName.name -Property Description
 $ADObject = Get-ADComputer -Identity $ComputerName -Property Description
 
 $a = Get-Date
@@ -30,4 +35,9 @@ $Description = "MD: $d; SN: $e; PO: $PO; $b"
 $TF = [string]::IsNullOrEmpty($ADObject.Description)
 
 # IF FALSE OR EMPTY THEN ADD DESCRIPTION, ELSE DO NOTHING
+# VERSION 2A: if ([string]::IsNullOrEmpty($ADObject.Description)){}
+# VERSION 2A: if (-not [string]::IsNullOrEmpty($ADObject.Description)){}
+# VERSION 2B: If ($TF -eq $True) {write-host "true AD desc is empty"} else {write-host "false AD desc exists"}
+# VERSION 2B: If ([string]::IsNullOrEmpty ($CurrDesc) ){write-host "true AD desc is empty"} else {write-host "false AD desc exists"}
+
 If($TF) {Set-ADComputer $env:COMPUTERNAME -Description "$Description"} else {}
